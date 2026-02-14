924eefa58df7eb239d8306edfdb130746d0dbb9f
Ahad287 <ahad80390@gmail.com>
2026-02-14 09:56:37 +0530

    Update firebase.js

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBTO0zOfpB7QN4v1p-dnLFMbgSY6khJ3V0",
   authDomain: "vango-food-del.firebaseapp.com",
@@ -14,7 +10,6 @@ const firebaseConfig = {
   appId: "1:173302152018:web:6123d15858bad5421c588b"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app)
 export {app,auth}
